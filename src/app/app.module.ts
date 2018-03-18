21acdda212ab034e92c349085a083d08dfbd2802
uijinLee <uijin95@gmail.com>
2018-03-18 14:04:55 +0900

    3.181

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,8 +22,9 @@ import { MyinfoPage } from '../pages/myinfo/myinfo';
 import { ElasticHeaderModule } from "ionic2-elastic-header/dist";
 import { SettingsPage } from '../pages/settings/settings'
 import { FirebaseProvider } from '../providers/firebase/firebase';
-import { AngularFireDatabaseModule } from 'angularfire2/database-deprecated';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AdminPage } from '../pages/admin/admin';
+import { AngularFireDatabase } from 'angularfire2/database'
 
 @NgModule({
   declarations: [
@@ -43,9 +44,9 @@ import { AdminPage } from '../pages/admin/admin';
     BrowserModule,
     IonicModule.forRoot(MyApp),
     AngularFireModule.initializeApp(environment.firebase),
-    AngularFireDatabaseModule,
     AngularFirestoreModule.enablePersistence(),
     AngularFireAuthModule,
+    AngularFireDatabaseModule,
     ElasticHeaderModule
   ],
   bootstrap: [IonicApp],
